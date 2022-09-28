72c6caaebb8e0cccaa48df1f942ec0119619ca73
Aishwarya <aishaishwarya914@gmail.com>
2022-09-28 16:56:10 +0530

    login

--- angular/src/app/modules/users/components/login/login.component.ts
+++ angular/src/app/modules/users/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { HttpServiceService } from '../../http-service.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  formNotValid: boolean = false
+  constructor(private http : HttpServiceService) { }
 
-  constructor() { }
+  loginForm : FormGroup = new  FormGroup({
+    username : new FormControl('' , Validators.required),
+    password : new FormControl("" , Validators.required)
+  })
+
+  submitLogin(){
+    if (this.loginForm.valid) {
+      this.http.loginUser(this.loginForm.value).subscribe((resp) => {
+        console.log(resp);
+      },
+        (error) => {
+          console.log(error.error);
+        }
+)
+    }
+    else {
+      console.log('fill properly ');
+      this.formNotValid = true
+      console.log(this.loginForm.valid);
+
+    }
+  }
 
   ngOnInit(): void {
   }
+
 
 }
