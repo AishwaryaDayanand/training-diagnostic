60889f7986a26199b9b60f85b83891cfb09684ff
Aishwarya <aishaishwarya914@gmail.com>
2022-09-28 15:52:12 +0530

    'initial'

---
+++ angular/src/app/modules/users/components/register-customer/register-customer.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { HttpServiceService } from '../../http-service.service';
+
+@Component({
+  selector: 'app-register-customer',
+  templateUrl: './register-customer.component.html',
+  styleUrls: ['./register-customer.component.css']
+})
+export class RegisterCustomerComponent implements OnInit {
+  formNotValid : boolean = false
+  formError ?: string =""
+  constructor(private http: HttpServiceService) { }
+
+  customerRegisterForm: FormGroup = new FormGroup({
+    username : new FormControl(" ", Validators.required),
+    first_name: new FormControl(" ", Validators.required),
+    last_name: new FormControl(" ", Validators.required),
+    email: new FormControl(" ", [Validators.required, Validators.email]),
+    mobile_number: new FormControl(" ", [Validators.maxLength(12), Validators.required]),
+    age: new FormControl(" ", Validators.required),
+    address: new FormControl(" "),
+    pincode: new FormControl(" ", Validators.maxLength(6)),
+    password: new FormControl(" ", Validators.minLength(8))
+  })
+  ngOnInit(): void {
+  }
+  submitRegister() {
+    // console.log(this.customerRegisterForm.value);
+    if (this.customerRegisterForm.valid) {
+      this.http.registerCustomer(this.customerRegisterForm.value).subscribe(data => console.log(data))
+    }
+    else {
+      console.log('fill properly ');
+      this.formNotValid = true
+      console.log(this.customerRegisterForm.valid);
+       
+    }
+    console.log(this.customerRegisterForm.value);
+
+  }
+}
---
+++ angular/src/app/modules/users/http-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  registerCustomer(data: any){
+    return this.http.post<any>('http://127.0.0.1:8000/users/register-customer/',data)
+  }
+  registerEmployee(data:any){
+    return this.http.post<any>('http://127.0.0.1:8000/users/register-employee/', data)
+
+  }
+  getUsers(){
+    return this.http.get('http://127.0.0.1:8000/users/register-customer/')
+  }
+  getBranches(){
+    return this.http.get('http://127.0.0.1:8000/users/branch/')
+
+  }
+
+  }
